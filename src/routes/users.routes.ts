2ea082ffd3729c90a24ff4d296fcf507d38c5bc7
nghiakieran <lechinghia202@gmail.com>
2025-06-14 13:49:59 +0700

    feat: add curried generic filter middleware for req.body sanitization

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -11,6 +11,7 @@ import {
   getMeController,
   updateMeController
 } from '~/controllers/users.controllers'
+import { filterMiddleware } from '~/middlewares/common.middlewares'
 import {
   accessTokenValidator,
   emailVerifyTokenValidator,
@@ -23,6 +24,7 @@ import {
   verifiedUserValidator,
   verifyForgotPasswordTokenValidator
 } from '~/middlewares/users.middlewares'
+import { UpdateMeReqBody } from '~/models/requests/User.requests'
 import { wrapRequestHandler } from '~/utils/handlers'
 
 const userRouter = Router()
@@ -48,6 +50,16 @@ userRouter.patch(
   accessTokenValidator,
   verifiedUserValidator,
   updateMeValidator,
+  filterMiddleware<UpdateMeReqBody>([
+    'name',
+    'date_of_birth',
+    'bio',
+    'location',
+    'website',
+    'username',
+    'avatar',
+    'cover_photo'
+  ]),
   wrapRequestHandler(updateMeController)
 )
 
